964147185bec99794bee72d98999051266a183e6
Mik <mik23@protonmail.ch>
2020-07-23 17:30:17 +0530

    updated for 3D pose keypoints

--- run.py
+++ run.py
@@ -7,13 +7,19 @@ from tf_pose import common
 import cv2
 import numpy as np
 from tf_pose.estimator import TfPoseEstimator
-from tf_pose.networks import get_graph_path, model_wh
+from tf_pose.networks import get_graph_path, model_whs
+
+# Modules for 3D Pose Dectection
+sys.path.append('./tf_pose/lifting')
+from tf_pose.lifting.prob_model import Prob3dPose
+from tf_pose.lifting.draw import plot_pose
 
 logger = logging.getLogger('TfPoseEstimator')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
@@ -21,13 +27,15 @@ logger.addHandler(ch)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation run')
     parser.add_argument('--image', type=str, default='./images/p1.jpg')
-    parser.add_argument('--model', type=str, default='cmu', help='cmu / mobilenet_thin')
+    parser.add_argument('--model', type=str, default='cmu',
+                        help='cmu / mobilenet_thin')
 
     parser.add_argument('--resize', type=str, default='0x0',
                         help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
                         help='if provided, resize heatmaps before they are post-processed. default=1.0')
-    parser.add_argument('--output_json', type=str, default='/tmp/', help='writing output json dir')
+    parser.add_argument('--output_json', type=str,
+                        default='/tmp/', help='writing output json dir')
     args = parser.parse_args()
 
     w, h = model_wh(args.resize)
@@ -42,12 +50,14 @@ if __name__ == '__main__':
         logger.error('Image can not be read, path=%s' % args.image)
         sys.exit(-1)
     t = time.time()
-    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+    humans = e.inference(image, resize_to_default=(
+        w > 0 and h > 0), upsample_size=args.resize_out_ratio)
     elapsed = time.time() - t
 
     logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
 
-    image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False, frame=0, output_json_dir=args.output_json)
+    image = TfPoseEstimator.draw_humans(
+        image, humans, imgcopy=False, frame=0, output_json_dir=args.output_json)
 
     import matplotlib.pyplot as plt
 
@@ -57,7 +67,8 @@ if __name__ == '__main__':
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
     bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
-    bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+    bgimg = cv2.resize(
+        bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
 
     # show network output
     a = fig.add_subplot(2, 2, 2)
@@ -82,3 +93,31 @@ if __name__ == '__main__':
     plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
     plt.colorbar()
     plt.show()
+
+    logger.info("3d lifting initialisation")
+    poseLifting = Prob3dPose("./tf_pose/lifting/models/prob_model_params.mat")
+
+    image_h, image_w = image.shape[:2]
+    standard_w, standard_h = 640, 480
+
+    pose_2d_mpiis = []
+    visibilities = []
+
+    for human in humans:
+        pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
+        pose_2d_mpiis.append([(int(x * standard_w + 0.5), int(y * standard_h + 0.5)) for x, y in pose_2d_mpii])
+        visibilities.append(visibility)
+    
+    pose_2d_mpiis = np.array(pose_2d_mpiis)
+    visibilities = np.array(visibilities)
+    transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
+    pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
+
+    print("3D Pose Keypoints \n", pose_3d)
+
+    for i, single_3d in enumerate(pose_3d):
+        plot_pose(single_3d)
+    plt.show()
+
+    logger.info("~END~")
+    pass
